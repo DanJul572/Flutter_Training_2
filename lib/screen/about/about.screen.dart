e23277645562acb6819b44afc2c87f9f80dc7308
Dandi <dandi.juliandi@nawadata.id>
2024-07-13 06:49:28 +0700

    add image picker in about screen

--- lib/screen/about/about.screen.dart
+++ lib/screen/about/about.screen.dart
@@ -1,7 +1,42 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class AboutScreen extends StatelessWidget {
+class AboutScreen extends StatefulWidget {
   const AboutScreen({super.key});
+
+  @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  File? _image;
+
+  final ImagePicker _picker = ImagePicker();
+
+  Widget buildImageContent() {
+    if (_image != null) {
+      return Image.file(
+        _image!,
+        fit: BoxFit.cover,
+      );
+    }
+    return Image.asset('assets/images/avatar.jpg');
+  }
+
+  Future<void> _getImage() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('No image selected')),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,16 +44,19 @@ class AboutScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox(
-            height: 150.0,
-            width: 150.0,
-            child: ClipOval(
-              child: Image.asset('assets/images/avatar.jpg'),
+          GestureDetector(
+            onTap: () {
+              _getImage();
+            },
+            child: SizedBox(
+              height: 150.0,
+              width: 150.0,
+              child: ClipOval(
+                child: buildImageContent(), // Placeholder image
+              ),
             ),
           ),
-          const SizedBox(
-              height: 20.0
-          ),
+          const SizedBox(height: 20.0),
           const Text(
             'User Name',
             style: TextStyle(color: Colors.deepOrange, fontSize: 25.0),
